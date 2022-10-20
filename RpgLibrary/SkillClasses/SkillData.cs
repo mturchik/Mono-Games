24e1b61eaf7caff2b180a14436ef34dca56ac0ca
mturchik <mturchik@batteriesplus.com>
2022-10-20 18:19:52 -0500

    Fully implement Skills data manager page

--- RpgLibrary/SkillClasses/SkillData.cs
+++ RpgLibrary/SkillClasses/SkillData.cs
@@ -2,13 +2,18 @@
 public class SkillData : BaseData
 {
     public string PrimaryAttribute { get; set; } = "";
-    public Dictionary<string, int> ClassModifiers { get; } = new();
+    public Dictionary<string, int> ClassModifiers { get; set; } = new();
 
     public override SkillData Clone()
     {
+        var modifiers = new Dictionary<string, int>();
+        foreach (var kv in ClassModifiers) modifiers.TryAdd(kv.Key, kv.Value);
         return new()
         {
             Id = Id,
+            Name = Name,
+            PrimaryAttribute = PrimaryAttribute,
+            ClassModifiers = modifiers
         };
     }
 }
